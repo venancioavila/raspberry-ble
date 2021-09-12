b3b94878cd5a8fc70153b806993bfb5513d3ec23
venancioavila <venanciodesouza@icloud.com>
2021-09-12 16:58:29 -0300

    first commit

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const bluetooth = require('node-bluetooth')
 const device = new bluetooth.DeviceINQ();
 
 device.listPairedDevices(console.log)
+
+device.on('finished', console.log(console, 'finished'))
+device.on('found', (address, name) => {
+    console.log({address, name})
+})
